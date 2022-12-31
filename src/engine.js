8940a9c26a78c933051082bb0085ef6a11411f4c
kanyamo <kanajir@yahoo.co.jp>
2022-12-31 15:41:55 +0900

    インデントとクラス定義を修正

--- src/engine.js
+++ src/engine.js
@@ -42,116 +42,112 @@ function create () {
 
 class Character extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-    super(scene, x, y, texture, frame);
+        super(scene, x, y, texture, frame);
 
+        // キャラクターのステータスを定義する
+        this.hp=100;
+        this.attack=10;
+        this.defense=5;
 
- // キャラクターのステータスを定義する
-this.hp=100;
-this.attack=10;
-this.defense=5;
-
- // キャラクターをシーンに追加する
-scene.add.existing(this);
-}
+        // キャラクターをシーンに追加する
+        scene.add.existing(this);
+    }
 
   // キャラクターがダメージを受けるメソッド
-takeDamage(damage) {
-    this.hp -= damage;
-    if (this.hp <= 0) {
-    this.die();
+    takeDamage(damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            this.die();
+        }
     }
-}
 
-// キャラクターが死亡するメソッド
-die() {
-    // キャラクターを削除する処理をここに記述する
-}
+    // キャラクターが死亡するメソッド
+    die() {
+        // キャラクターを削除する処理をここに記述する
+    }
 }
 
 class MyScene extends Phaser.Scene {
     constructor() {
         super({ key: 'my_scene' });
     }
-}
-    create(); {
 
- // キャラクターを配置する
-this.player = new Character(this, 100, 100, "player", 0);
-this.enemy = new Character(this, 300, 100, "enemy", 0);
+    create() {
+        // キャラクターを配置する
+        this.player = new Character(this, 100, 100, "player", 0);
+        this.enemy = new Character(this, 300, 100, "enemy", 0);
 
-// ターンを制御するタイマーを作成する
-this.turnTimer = this.time.addEvent({
-    delay: 1000, // 1秒ごとにターンを切り替える
-    callback: this.nextTurn,
-    callbackScope: this,
-    loop: true,
-});
+        // ターンを制御するタイマーを作成する
+        this.turnTimer = this.time.addEvent({
+            delay: 1000, // 1秒ごとにターンを切り替える
+            callback: this.nextTurn,
+            callbackScope: this,
+            loop: true,
+        });
 
- // 入力を受け付けるためのハンドラーを設定する
-this.input.keyboard.on("keydown", this.onKeyDown, this);
+        // 入力を受け付けるためのハンドラーを設定する
+        this.input.keyboard.on("keydown", this.onKeyDown, this);
 
- // 戦闘終了を判定するための変数を初期化する
-this.isBattleEnd = false;
+        // 戦闘終了を判定するための変数を初期化する
+        this.isBattleEnd = false;
     }
 
-// シーンがアップデートされるたびに呼ばれるメソッド
-update(time, delta); {
-    // キャラクター同士の衝突を判定する処理をここに記述する
+    // シーンがアップデートされるたびに呼ばれるメソッド
+    update(time, delta) {
+        // キャラクター同士の衝突を判定する処理をここに記述する
 
-// 戦闘が終了している場合は処理を終了する
-if (this.isBattleEnd) {
-    return;
-    }
+        // 戦闘が終了している場合は処理を終了する
+        if (this.isBattleEnd) {
+            return;
+        }
 
-// プレイヤーのHPが0以下の場合、敵の勝利として戦闘を終了する
-if (this.player.hp <= 0) {
-    this.endBattle("enemy");
-    return;
-    }
-}
-
-
-
-// 次のターンへ進むメソッド
-nextTurn(); {
-    // 現在のターンを切り替える
-    this.currentTurn = this.currentTurn === "player" ? "enemy" : "player";
-
-     // 現在のターンのキャラクターを取得する
-    const currentCharacter = this[this.currentTurn];
-  // 現在のターンのキャラクターが行動できるか判定する
-if (currentCharacter.canAct()) {
-    // 行動できる場合、アクションを実行する
-    currentCharacter.act();
-} else {
-    // 行動できない場合、次のターンへ進む
-    this.nextTurn();
+        // プレイヤーのHPが0以下の場合、敵の勝利として戦闘を終了する
+        if (this.player.hp <= 0) {
+            this.endBattle("enemy");
+            return;
         }
     }
- // キーが押されたときに呼ばれるハンドラー
-onKeyDown(event); {
-    // 現在のターンのキャラクターを取得する
-    const currentCharacter = this[this.currentTurn];
 
-    // 現在のターンのキャラクターが行動できるか判定する
-    if (currentCharacter.canAct()) {
-      // 行動できる場合、アクションを実行する
-currentCharacter.act(event.keyCode);
+    // 次のターンへ進むメソッド
+    nextTurn() {
+        // 現在のターンを切り替える
+        this.currentTurn = this.currentTurn === "player" ? "enemy" : "player";
+
+        // 現在のターンのキャラクターを取得する
+        const currentCharacter = this[this.currentTurn];
+        // 現在のターンのキャラクターが行動できるか判定する
+        if (currentCharacter.canAct()) {
+            // 行動できる場合、アクションを実行する
+            currentCharacter.act();
+        } else {
+            // 行動できない場合、次のターンへ進む
+            this.nextTurn();
+        }
     }
 
-// 戦闘を終了するメソッド
-endBattle(winner); {
-    // 戦闘終了を判定する変数を更新する
-    this.isBattleEnd = true;
+    // キーが押されたときに呼ばれるハンドラー
+    onKeyDown(event) {
+        // 現在のターンのキャラクターを取得する
+        const currentCharacter = this[this.currentTurn];
 
-    // 勝者を表示する
-    console.log(`${winner}の勝利！`);
-    // シーンを遷移する
-this.scene.start("GameOverScene", { winner: winner });
+        // 現在のターンのキャラクターが行動できるか判定する
+        if (currentCharacter.canAct()) {
+        // 行動できる場合、アクションを実行する
+    currentCharacter.act(event.keyCode);
+        }
+    }
+
+    // 戦闘を終了するメソッド
+    endBattle(winner) {
+        // 戦闘終了を判定する変数を更新する
+        this.isBattleEnd = true;
+
+        // 勝者を表示する
+        console.log(`${winner}の勝利！`);
+        // シーンを遷移する
+        this.scene.start("GameOverScene", { winner: winner });
     }
 }
-
-
 
 
 // フレームごとに呼ばれる関数
